b89ebea05caeaa6e22979eafa8c53ee463360172
mahmoud-sayed <mahmoudalsayied@gmail.com>
2023-03-19 18:11:19 +0200

    done making login and register form but need some modifcations

--- src/Pages/register/Register.js
+++ src/Pages/register/Register.js
@@ -1,10 +1,53 @@
 import React from 'react';
-import * as Yup from 'yup';
-import { Formik, Form } from 'formik';
+import { Link } from 'react-router-dom';
+import './Register-style.scss';
+import image from "./../../assets/Login-Register/img-login.svg";
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="login">
+      <div className="login__content">
+        <div className="login__img">
+          <img src={image} alt="" />
+        </div>
+        <div className="login__forms">
+          <form action="" className="login__registre" id="login-in">
+            <h1 className="login__title">Sign Up</h1>
+
+            <div className="login__box">
+              <i className='bx bx-user login__icon'></i>
+              <input type="text" placeholder="Username" className="login__input" />
+            </div>
+
+            <div className="login__box">
+              <i className='bx bx-user login__icon'></i>
+              <input type="email" placeholder="Email" className="login__input" />
+            </div>
+
+            <div className="login__box">
+              <i className='bx bx-lock-alt login__icon'></i>
+              <input type="password" placeholder="Password" className="login__input" />
+            </div>
+
+            <div className="login__box">
+              <i className='bx bx-lock-alt login__icon'></i>
+              <input type="password" placeholder="Confirm Password" className="login__input" />
+            </div>
+
+            <a href="#" className="login__forgot">Forgot password?</a>
+
+            <a href="#" className="login__button">Sign In</a>
+
+            <div>
+              <span className="login__account">Don't have an Account ?</span>
+              <Link to='/login' className="login__signin" id="sign-up">Sign In</Link>
+            </div>
+          </form>
+
+
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/Pages/signin/SignIn.js
+++ src/Pages/signin/SignIn.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import './signIn-style.scss';
+import { Link } from 'react-router-dom';
+import image from "./../../assets/Login-Register/img-login.svg";
+
 
 const SignIn = () => {
+
   return (
-    <div>SignIn</div>
+    <div className="login">
+      <div className="login__content">
+        <div className="login__img">
+          <img src={image} alt="" />
+        </div>
+        <div className="login__forms">
+          <form action="" className="login__registre" id="login-in">
+            <h1 className="login__title">Sign In</h1>
+            <div className="login__box">
+              <i className='bx bx-user login__icon'></i>
+              <input type="text" placeholder="Username" className="login__input" />
+            </div>
+            <div className="login__box">
+              <i className='bx bx-lock-alt login__icon'></i>
+              <input type="password" placeholder="Password" className="login__input" />
+            </div>
+            <a href="#" className="login__forgot">Forgot password?</a>
+            <a href="#" className="login__button">Sign In</a>
+            <div>
+              <span className="login__account">Don't have an Account ?</span>
+              <Link to='/register' className="login__signin" id="sign-up">Sign Up</Link>
+            </div>
+          </form>
+
+
+        </div>
+      </div>
+    </div>
   );
 };
 
